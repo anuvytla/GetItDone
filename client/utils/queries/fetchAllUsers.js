58e1ed0dc82f23821f945da603ebcfbf07b57435
Max McLaughlin <maxwelljmack@gmail.com>
2022-09-27 20:15:22 -0700

    created the basics of the backend. need to make seeding, but database seems to be connected, server runs. npm run dev also work.

---
+++ client/utils/queries/fetchAllUsers.js
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client";
+
+export const FETCH_USERS_QUERY = gql`
+	query Users {
+		users {
+			_id
+			username
+			email
+		}
+	}
+`;
---
+++ server/schema/typeDefs.js
@@ -0,0 +1,18 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+	type User {
+		_id: ID
+		username: String
+		email: String
+	}
+
+	type Query {
+		users: [User]
+	}
+	type Mutation {
+		createUser(username: String!, email: String!, password: String!): User
+	}
+`;
+
+module.exports = typeDefs;
