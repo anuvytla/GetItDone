f9cb6b47c04b9132ed7480ec35ac313ce2b8e187
Max McLaughlin <maxwelljmack@gmail.com>
2022-10-01 10:13:05 -0700

    added references to models + project model

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,20 +1,14 @@
 const { Schema, model } = require("mongoose");
 
-const taskSchema = new Schema({
+const ProjectSchema = new Schema({
 	id: Number,
 	title: String,
 	description: String,
-	urgency: Number,
 	// probably add a user_id to this that refers to user id
 	userId: {
 		type: Schema.Types.ObjectId,
 		ref: "User",
 	},
-	// also need to associate each task with a board by board id
-	boardId: {
-		type: Schema.Types.ObjectId,
-		ref: "TaskBoard",
-	},
 });
 
-module.exports = model("Task", taskSchema);
+module.exports = model("Project", ProjectSchema);
