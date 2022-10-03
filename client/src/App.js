83e0202d9f05a113e7dcb866251d8e0fbb8b9a9c
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-10-03 13:39:20 -0700

    Add side bar with navigation, implement routes and add task buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import {Route, Routes } from 'react-router-dom';
 import Dashboard from './Components/Dashboard';
+import Sidebar from './Components/Sidebar';
+import Donation from './Components/Donation';
+import Notifications from './Components/Notifications';
+
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard/>
-    </div>
+    <>
+        <Routes>
+          <Route path="/" element={<Sidebar />}>
+            <Route index element={<Dashboard />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/donation' element={<Donation />} />
+            <Route path='/notifications' element={<Notifications />} />
+          </Route>
+        </Routes>
+    </>
   );
 }
 
--- client/src/Components/AddTask/index.js
+++ client/src/Components/AddTask/index.js
@@ -1,9 +1,32 @@
-import './index.css'
+import './index.css';
+import { useState } from 'react';
 
 const AddTask = () => {
+    const [newTask, setNewTask] = useState('');
+    const [task, setTask] = useState([]);
     return (
         <>
-        <button>Add Task</button>
+            <label htmlFor='newTask'> New Task</label>
+            <input
+                id='newTask'
+                value={newTask}
+                onChange={event => setNewTask(event.target.value)} />
+
+            <button className='newInput'
+                onClick={() => {
+                    if (newTask.trim().length === 0) {
+                        alert('Must enter valid task!');
+                        return;
+                    }
+
+                    task.push(newTask);
+                    setTask(task);
+                }}>
+                Add Task
+            </button>
+                
+
+                 
         </>
     )
 }
