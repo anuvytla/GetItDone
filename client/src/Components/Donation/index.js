77f9b7dc8ca3c7f3045ef7803d1c0d253a42d3b2
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-10-04 16:05:08 -0700

    implement donation page using stripe

--- client/src/Components/Donation/index.js
+++ client/src/Components/Donation/index.js
@@ -1,9 +1,55 @@
 import './index.css';
+import { loadStripe } from "@stripe/stripe-js";
+import {
+    Elements,
+    useElements,
+    useStripe,  
+    PaymentElement,
+  } from "@stripe/react-stripe-js";
+  
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Donation = () => {
+    return (
+        <Elements stripe={stripePromise}>
+            <DonationForm/>
+        </Elements>
+    )
+}
+
+const DonationForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        if (!stripe || !elements) {
+          return;
+        }
+    
+        const result = await stripe.confirmPayment({
+          elements,
+          confirmParams: {
+            return_url: "https://www.google.com",
+          },
+        });
+    
+        if (result.error) {
+          console.log(result.error.message);
+        } else {
+
+        }
+      };
+    
+
     return(
         <>
-            <h1>Hello World!</h1>
+        <form onSubmit={handleSubmit}>
+            <PaymentElement />
+            <button disabled={!stripe}>Donate</button>
+        </form>
         </>
     )
 }
