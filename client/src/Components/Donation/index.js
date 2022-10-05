2d6c5ed1885e9a7edab658df918df3af487bc217
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-10-04 21:26:44 -0700

    Implement backend for stripe payment

--- client/src/Components/Donation/index.js
+++ client/src/Components/Donation/index.js
@@ -3,12 +3,13 @@ import { loadStripe } from "@stripe/stripe-js";
 import {
     Elements,
     useElements,
-    useStripe,  
-    PaymentElement,
+    useStripe, 
+    CardElement 
   } from "@stripe/react-stripe-js";
+  import { useState } from 'react';
   
 
-const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+const stripePromise = loadStripe('pk_test_51LpO6EAdB0A1DfZyIYEp2su4jFGm8NsleOXZaggin50n6T36fg2oHsMFxECbmsdAUyt0wgOZTywZ9m6qHWv6Ms3i00tyQ5wI8A');
 
 const Donation = () => {
     return (
@@ -19,39 +20,76 @@ const Donation = () => {
 }
 
 const DonationForm = () => {
+    const [succeeded, setSucceeded] = useState(false);
+    const [error, setError] = useState(null);
+    const [processing, setProcessing] = useState("");
+    const [donationAmount, setDonationAmount] = useState(0);
+    const [disabled, setDisabled] = useState(true);
+
     const stripe = useStripe();
     const elements = useElements();
+    
+    const handleChange = async (event) => {
+      // Listen for changes in the CardElement and display any errors as the customer types their card details
+      setDisabled(event.empty);
+      setError(event.error ? event.error.message : "");
+    };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-    
-        if (!stripe || !elements) {
-          return;
-        }
-    
-        const result = await stripe.confirmPayment({
-          elements,
-          confirmParams: {
-            return_url: "https://www.google.com",
-          },
-        });
-    
-        if (result.error) {
-          console.log(result.error.message);
-        } else {
+    const handleSubmit = async (ev) => {
+      ev.preventDefault();
+      setProcessing(true);
 
-        }
-      };
-    
+      const res = await fetch("/payment-intent", {
+                                method: "POST",
+                                headers: {
+                                  "Content-Type": "application/json",
+                                },
+                                body: JSON.stringify({ amount: donationAmount }),
+                              });
+      const paymentIntentJSON = await res.json();
+      const clientSecret = paymentIntentJSON.clientSecret;
 
-    return(
-        <>
-        <form onSubmit={handleSubmit}>
-            <PaymentElement />
-            <button disabled={!stripe}>Donate</button>
-        </form>
-        </>
-    )
+    // Confirm Card Payment.                                   
+      const payload = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      });
+      if (payload.error) {
+        setError(`Payment failed ${payload.error.message}`);
+        setProcessing(false);
+      } else {
+        setError(null);
+        setProcessing(false);
+        setSucceeded(true);
+      }
+    };
+
+    //  Donate UI.
+    return (
+      <form id="donation-form" onSubmit={handleSubmit}>
+        <input type='number' step='1' placeholder='2' id='donation-amount' value={donationAmount} 
+                onChange={event => setDonationAmount(parseInt(event.target.value))}
+          />
+        <CardElement 
+          id="card-element"
+          options={{}} 
+          onChange={handleChange}
+        />
+        <button disabled={processing || disabled || succeeded} id="submit">
+          <span id="button-text">
+            {processing ? <div className="spinner" id="spinner"></div> : "Donate"}
+          </span>
+        </button>
+        {/* Show any error that happens when processing the payment */}
+        {error && (
+          <div className="card-error" role="alert">{error}</div>
+        )}
+        {/* Show a success message upon completion */}
+        <p className={succeeded ? "result-message" : "result-message hidden"}>Thank you for the donation!</p>
+      </form>
+    );
 }
+
 
 export default Donation;
