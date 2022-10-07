dd03d0be7f496b07464bcbb11b679ac53d7436a1
Max McLaughlin <maxwelljmack@gmail.com>
2022-10-06 20:08:29 -0700

    query fixes and added mutation/query access to front end

--- client/src/utils/queries/queries.js
+++ client/src/utils/queries/queries.js
@@ -1,12 +1,12 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const QUERY_TASKS_IN_BOARD = gql`
-  query tasksInBoard($boardId: ID!) {
-    tasks(boardId: $boardId) {
-      _id
-      title
-      description
-      status
-    }
-  }
+	query TasksById($boardId: String!) {
+		tasksById(boardId: $boardId) {
+			title
+			description
+			status
+			_id
+		}
+	}
 `;
