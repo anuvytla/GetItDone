51db35423a1f46be7a75913ea23f5101642baca6
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-10-08 09:47:36 -0700

    Update Task componenet

--- client/src/Components/Task/index.js
+++ client/src/Components/Task/index.js
@@ -1,29 +1,26 @@
 import "./index.css"
 import { Draggable } from "react-beautiful-dnd";
-import { useQuery } from "@apollo/client";
 import { QUERY_TASK_STATUS } from "../../utils/queries/queries";
 
 const Task = ({task, index}) => {
-    const { isLoading, data } = useQuery(QUERY_TASK_STATUS, {
-        variables: { taskId: task._id },
-      });
-      const status = data?.taskStatus.status || 'To Do';
+      const status = task.status || 'To Do';
       
-      let bgColor = '#f7887d'
+      let bgClass = 'card-todo';
       if (status === "Doing") {
-        bgColor = "#ffda61"
+        bgClass = 'card-doing';
       } else if (status === "Done"){
-        bgColor = "#68d89b"
+        bgClass = 'card-done';
       }
+
+      let cName = 'card '+bgClass;
 
     return (
         <Draggable draggableId={task._id} index={index}>
             {(provided) => (
-                <div className='card'
+                <div className={cName}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
-                    ref={provided.innerRef}
-                    style={{backgroundColor: bgColor}}>
+                    ref={provided.innerRef}>
                     <h5>{task.title}</h5>
                     <p>{task.description}</p>
                 </div>
